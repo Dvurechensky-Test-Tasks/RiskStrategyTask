b080da1abb275ebffd5ba41a1e54173fb01d3e70
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2024-10-05 06:58:11 +0300

    1.0
    Main

---
+++ Companies/Companies/ScriptsAndCss/TypeScripts/main.ts
@@ -0,0 +1,32 @@
+﻿(() => {
+    //по загрузке страницы
+    window.addEventListener("load", () => {
+        //создаем экземпляр класса утилит
+        const utilities = new Utilities();
+        //создаем экземпляр класса кук
+        const cookies = new Cookies();
+        //получаем текущий URL
+        const currentUrl = new URL(document.location.href);
+        //получаем путь из URL
+        const pathname = currentUrl.pathname.toLowerCase();
+        //разбиваем пути URL на части
+        const partsPath = pathname.split("/");
+        switch (partsPath[1]) { //смотрим путь URL
+            case "":
+                {
+                    console.log("Index Load");
+                    new Index();
+                }
+                break;
+            case "details": //страница детализации
+                {   //создаем класс авторизации кабинета
+                    console.log("Details Load");
+                    const id = partsPath[2];
+                    const detailsScene = new Details(utilities, cookies, id);
+                    //запускаем главную сцену
+                    detailsScene.startDetailsLogic();
+                }
+                break;
+        }
+    });
+})();
