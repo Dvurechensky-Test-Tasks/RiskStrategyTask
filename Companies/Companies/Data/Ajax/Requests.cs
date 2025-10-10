162d67a82cfca17cadc224e05a911a71ec070f8f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-10 06:50:26 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-10

--- Companies/Companies/Data/Ajax/Requests.cs
+++ Companies/Companies/Data/Ajax/Requests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 октября 2025 06:51:23
- * Version: 1.0.59
+ * Last Updated: 10 октября 2025 06:50:24
+ * Version: 1.0.60
  */
 
 using Companies.Data.Home;
