157191e12ded98c0d601d5b379258c2729762b31
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-21 14:55:50 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-21

--- Companies/Companies/Controllers/HomeController.cs
+++ Companies/Companies/Controllers/HomeController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 октября 2025 14:19:10
- * Version: 1.0.71
+ * Last Updated: 21 октября 2025 14:55:49
+ * Version: 1.0.72
  */
 
 using Companies.Data.Home;
