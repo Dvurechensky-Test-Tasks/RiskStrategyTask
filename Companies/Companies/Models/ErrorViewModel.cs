f40ea78485a296fbde1c61b95039a62333300fb6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-26 12:22:51 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-26

--- Companies/Companies/Models/ErrorViewModel.cs
+++ Companies/Companies/Models/ErrorViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 октября 2025 06:50:25
- * Version: 1.0.76
+ * Last Updated: 26 октября 2025 12:22:50
+ * Version: 1.0.77
  */
 
 namespace Companies.Models
