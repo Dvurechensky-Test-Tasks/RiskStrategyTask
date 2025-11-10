ae0f45b5b77500d109dad40f02f6fbe3457d383e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-10 06:50:26 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-10

--- Companies/Companies/Data/Home/Company.cs
+++ Companies/Companies/Data/Home/Company.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 ноября 2025 06:50:46
- * Version: 1.0.91
+ * Last Updated: 10 ноября 2025 06:50:24
+ * Version: 1.0.92
  */
 
 using System.ComponentModel.DataAnnotations.Schema;
