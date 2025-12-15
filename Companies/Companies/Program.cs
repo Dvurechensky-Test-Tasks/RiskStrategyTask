e0e2b42f643f7e7097402bddb0eca19702d63adb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-15 06:50:26 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-15

--- Companies/Companies/Program.cs
+++ Companies/Companies/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 декабря 2025 14:32:34
- * Version: 1.0.126
+ * Last Updated: 15 декабря 2025 06:50:24
+ * Version: 1.0.127
  */
 
 using Companies.Services;
