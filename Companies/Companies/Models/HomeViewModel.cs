756b26b1611a1af2669f0a816714df389677612c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-25 09:29:08 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-25

--- Companies/Companies/Models/HomeViewModel.cs
+++ Companies/Companies/Models/HomeViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 декабря 2025 09:27:45
- * Version: 1.0.136
+ * Last Updated: 25 декабря 2025 09:29:07
+ * Version: 1.0.137
  */
 
 using Companies.Data.Home;
