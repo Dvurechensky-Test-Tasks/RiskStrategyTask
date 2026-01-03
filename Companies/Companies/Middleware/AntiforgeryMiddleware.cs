c2e8a677bd78ab5cbb04fb9f601f90edbf09ec2d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-03 14:58:22 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-03

--- Companies/Companies/Middleware/AntiforgeryMiddleware.cs
+++ Companies/Companies/Middleware/AntiforgeryMiddleware.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 января 2026 11:04:22
- * Version: 1.0.145
+ * Last Updated: 03 января 2026 14:58:20
+ * Version: 1.0.146
  */
 
 using Microsoft.AspNetCore.Antiforgery;
