01ab99e4add6c4f140b6be8dc75e99648512ea12
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-09 06:50:27 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-09

--- Companies/Companies/Extensions/AuthExt.cs
+++ Companies/Companies/Extensions/AuthExt.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 марта 2026 09:54:20
- * Version: 1.0.210
+ * Last Updated: 09 марта 2026 06:50:26
+ * Version: 1.0.211
  */
 
 namespace Extensions;
