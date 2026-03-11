e75f471fb37549869cf478a76de69d04d807adad
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-11 06:50:27 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-11

--- Companies/Companies/Controllers/AjaxController.cs
+++ Companies/Companies/Controllers/AjaxController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 марта 2026 09:13:21
- * Version: 1.0.212
+ * Last Updated: 11 марта 2026 06:50:25
+ * Version: 1.0.213
  */
 
 using Companies.Data.Ajax;
